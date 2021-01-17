fa7ea06a652e1091d3b64d14e421bed8487de9cc
gandis <gandis0713@gmail.com>
2021-01-17 19:35:34 +0900

    add event

--- dev/src/common/rendering/core/index.ts
+++ dev/src/common/rendering/core/index.ts
@@ -1,10 +1,12 @@
 import camera from './camera';
+import event from './event';
 import renderer from './renderer';
 import Shader from './Shader';
 import window from './window';
 
 export default {
   camera,
+  event,
   renderer,
   Shader,
   window,
