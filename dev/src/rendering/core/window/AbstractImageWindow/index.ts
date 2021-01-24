77a07d00d7fba86e4d773ce790fa6086e6235515
gandis <gandis0713@gmail.com>
2021-01-24 23:47:47 +0900

    modified render window

--- dev/src/rendering/core/window/AbstractImageWindow/index.ts
+++ dev/src/rendering/core/window/AbstractImageWindow/index.ts
@@ -10,12 +10,6 @@ abstract class AbstractImageWindow extends AbstractWindow {
     const renderer = this.renderer as AbstractImageRenderer;
     renderer.setImage(image);
   }
-
-  public initRenderer(width: number, height: number): void {
-    this.renderer.setShaderParameter();
-    this.renderer.createBuffer();
-    this.renderer.setSize(width, height);
-  }
 }
 
 export default AbstractImageWindow;
