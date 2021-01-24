be08dbb3ac697d052c6c94006ff3def00a2b3372
gandis <gandis0713@gmail.com>
2021-01-24 20:30:28 +0900

    apply touch event

--- dev/src/common/rendering/core/window/AbstractWindow/index.ts
+++ dev/src/common/rendering/core/window/AbstractWindow/index.ts
@@ -16,12 +16,22 @@ abstract class AbstractWindow {
     this.container.addEventListener('mousedown', this.mouseDownEvent, false);
     this.container.addEventListener('mousemove', this.mouseMoveEvent, false);
     this.container.addEventListener('mouseup', this.mouseUpEvent, false);
+
+    this.container.addEventListener('touchstart', this.touchStartEvent, false);
+    this.container.addEventListener('touchmove', this.touchMoveEvent, false);
+    this.container.addEventListener('touchend', this.touchEndEvent, false);
+    this.container.addEventListener('touchcancel', this.touchCancelEvent, false);
   }
 
   protected bind(): void {
     this.mouseDownEvent = this.mouseDownEvent.bind(this);
     this.mouseMoveEvent = this.mouseMoveEvent.bind(this);
     this.mouseUpEvent = this.mouseUpEvent.bind(this);
+
+    this.touchStartEvent = this.touchStartEvent.bind(this);
+    this.touchMoveEvent = this.touchMoveEvent.bind(this);
+    this.touchEndEvent = this.touchEndEvent.bind(this);
+    this.touchCancelEvent = this.touchCancelEvent.bind(this);
   }
 
   public initialize(width: number, height: number): void {
@@ -40,6 +50,22 @@ abstract class AbstractWindow {
   }
 
   protected mouseUpEvent(event): void {
+    this.renderer.mouseUpEvent(event);
+  }
+
+  protected touchStartEvent(event): void {
+    this.renderer.mouseDownEvent(event);
+  }
+
+  protected touchMoveEvent(event): void {
+    this.renderer.mouseMoveEvent(event);
+  }
+
+  protected touchEndEvent(event): void {
+    this.renderer.mouseUpEvent(event);
+  }
+
+  protected touchCancelEvent(event): void {
     this.renderer.mouseUpEvent(event);
   }
 
